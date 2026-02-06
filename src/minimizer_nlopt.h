a8248236918c4d2d9e7633a924991869fde50f14
Diana Morzy <diana.morzy@unibas.ch>
2026-02-06 10:11:45 +0100

    Continue past faulty cell instead of throwing

--- src/minimizer_nlopt.h
+++ src/minimizer_nlopt.h
@@ -94,7 +94,7 @@ Although the tolerance criterium was not met, the last valid step is used for pa
 
     catch(std::exception &e) {
         _file_log << "(minimize_wrapper) ERROR: Log likelihood optimization failed (" << e.what() << ")" << std::endl;
-        throw;
+        continue;
     }
     _save_ll = false; // stop ll output
 
